32d54c776ba3afb60e7884584dab66e27ec1162b
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-06-30 02:09:58 -0300

    more logs

--- src/main/java/com/example/demo/services/AssignProjectRestController.java
+++ src/main/java/com/example/demo/services/AssignProjectRestController.java
@@ -7,8 +7,11 @@ package com.example.demo.services;
 
 import com.example.demo.Employee.Employee;
 import com.example.demo.Employee.EmployeeRepository;
+import com.example.demo.Log.Log;
+import com.example.demo.Log.LogRepository;
 import com.example.demo.project.Project;
 import com.example.demo.project.ProjectRepository;
+import java.util.Date;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,14 +30,19 @@ public class AssignProjectRestController {
 
     private EmployeeRepository employeeRepository;
     private ProjectRepository projectRepository;
+    private LogRepository logRepository;
 
-    public AssignProjectRestController(EmployeeRepository employeeRepository, ProjectRepository projectRepository) {
+    public AssignProjectRestController(EmployeeRepository employeeRepository, ProjectRepository projectRepository, LogRepository logRepository) {
         this.employeeRepository = employeeRepository;
         this.projectRepository = projectRepository;
+        this.logRepository = logRepository;
     }
 
     @RequestMapping(method = POST, consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Employee> assign(@PathVariable Long employeeId, @RequestBody AssignDTO input) {
+        logRepository.save(new Log(""
+                + "Assigned project: " + input.getProjId()
+                + " to employee:" + employeeId, new Date()));
         Employee employee = employeeRepository.findOne(
                 employeeId);
         Project project = projectRepository.findOne(input.getProjId());
--- src/main/java/com/example/demo/services/AssignedEmployeesRestController.java
+++ src/main/java/com/example/demo/services/AssignedEmployeesRestController.java
@@ -7,6 +7,9 @@ package com.example.demo.services;
 
 import com.example.demo.Employee.Employee;
 import com.example.demo.Employee.EmployeeRepository;
+import com.example.demo.Log.Log;
+import com.example.demo.Log.LogRepository;
+import java.util.Date;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +24,17 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class AssignedEmployeesRestController {
 
     private final EmployeeRepository employeeRepository;
+    private final LogRepository logRepository;
 
-    public AssignedEmployeesRestController(EmployeeRepository employeeRepository) {
+    public AssignedEmployeesRestController(EmployeeRepository employeeRepository, LogRepository logRepository) {
         this.employeeRepository = employeeRepository;
+        this.logRepository = logRepository;
     }
 
     @RequestMapping(method = GET)
     public List<Employee> listAssigned() {
+        logRepository.save(new Log("Listed all employees assigned to a project",
+                 new Date()));
         List<Employee> employees = employeeRepository.findAll();
         employees.removeIf(el -> {
             if (el.getProjects().isEmpty()) {
